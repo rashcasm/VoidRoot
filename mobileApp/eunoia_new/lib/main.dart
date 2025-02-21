6216a551c1a5eec6eb8f16b57d83c9a8b2d6ef5a
Swayam Takkamore <70209059+SwayamTakkamore@users.noreply.github.com>
2025-02-21 12:08:44 +0530

    updating mobile app

--- mobileApp/eunoia_new/lib/main.dart
+++ mobileApp/eunoia_new/lib/main.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
-
-  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-    statusBarColor: Colors.transparent,
-    statusBarIconBrightness: Brightness.dark,
-    systemNavigationBarColor: Colors.white,
-    systemNavigationBarIconBrightness: Brightness.dark,
-  ));
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
@@ -36,7 +37,89 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        width: double.infinity,
+        child: Stack(
+          children: [
+            SafeArea(
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Container(
+                          height: 50,
+                          width: 50,
+                          child: Icon(Icons.menu),
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlueAccent,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ),
+                      IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            bottom_appbar(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Align bottom_appbar() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          height: 60,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.lightBlueAccent,
+            borderRadius: BorderRadius.all(Radius.circular(25)),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(onPressed: null, child: Icon(Icons.home, color: Colors.black)),
+              IconButton(
+                onPressed: () {
+                  print("search button pressed");
+                },
+                icon: Icon(Icons.search),
+              ),
+              IconButton(
+                onPressed: () {
+                  print("add button pressed");
+                },
+                icon: Icon(Icons.add),
+              ),
+              IconButton(
+                onPressed: () {
+                  print("favorite button pressed");
+                },
+                icon: Icon(Icons.favorite),
+              ),
+              IconButton(
+                onPressed: () {
+                  print("person button pressed");
+                },
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
